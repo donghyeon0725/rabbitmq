fc22d9e2fbc144f4260bc47e335a221ed43a6332
donghyeon0725 <ehdgus5015@gmail.com>
2021-10-06 00:40:54 +0900

    Initial commit

---
+++ src/main/java/com/study/rabbitmq/Send.java
@@ -0,0 +1,35 @@
+package com.study.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+            for (int i = 0; i <= 1000; i++) {
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                String message = "Hello World!" + (int) (Math.random() * 100);
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println(" [x] Set '" + message + "'");
+                Thread.sleep(10);
+            }
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
